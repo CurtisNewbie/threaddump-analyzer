3f518196bf055ad08bf05d755acf1da52e973517
yongj.zhuang <yongj.zhuang@outlook.com>
2023-12-20 16:02:23 +0800

    Change Build script, build for arm64/macos, amd64/linux

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o threaddump-analyzer
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o threaddump-analyzer-arm64-macos
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o threaddump-analyzer-amd64-linux
 
