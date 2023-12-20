f1f677c8986dda502547ad4b95b7d1d02302d7b2
yongj.zhuang <yongj.zhuang@outlook.com>
2023-12-20 22:57:14 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o threaddump-analyzer-arm64-macos
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o threaddump-analyzer-amd64-linux
+ver=$(git describe --tags --abbrev=0)
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o threaddump-analyzer-arm64-macos_$ver
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o threaddump-analyzer-amd64-linux_$ver
 
